62ec7e809fdb29ef35dd928726bbcc0067dafdd8
junkyahd <junkyahd@protonmail.com>
2023-10-24 23:43:10 -0400

    ...

---
+++ content/charts.md
@@ -0,0 +1,10 @@
+Title: Pendle Dashboard 
+Date: 2023-10-24 12:31
+Category: Analysis
+
+![Photo](/images/util.png)
+![Photo](/images/pt_price.png)
+![Photo](/images/yt_price.png)
+![Photo](/images/longYield.png)
+![Photo](/images/underlying_implied.png)
+
--- runScript2.py
+++ runScript2.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime, time, date
+from datetime import timedelta, datetime, date
 import requests
 import pickle
 import matplotlib.pyplot as plt
@@ -6,6 +6,8 @@ import seaborn as sns
 import math
 import numpy as np
 import pandas as pd
+from matplotlib import ticker
+from matplotlib.ticker import AutoMinorLocator
 
 addr = '0xc9becdbc62efb867cb52222b34c187fb170379c6'
 
@@ -13,7 +15,6 @@ end = datetime.strftime(date.today() + timedelta(days=1), '%Y-%m-%d')
 start = datetime.strftime(date.today() - timedelta(days=7), '%Y-%m-%d')
 timeFrame = 'hour'
 
-# u1 = 'https://api-v2.pendle.finance/core/v2/1/markets/0xc9becdbc62efb867cb52222b34c187fb170379c6/history?time_frame=hour&timestamp_start=2023-10-19&timestamp_end=2023-10-20'
 u = 'https://api-v2.pendle.finance/core/v2/1/markets/' + addr + '/history?time_frame=' + timeFrame + '&timestamp_start=' + start + '&timestamp_end=' + end
 
 r = requests.get(u)
@@ -49,6 +50,8 @@ for j in tmpDict.keys():
     util_ts.append(datetime.fromtimestamp(tmpDict[j][7]).isoformat())
     crvUtil.append(tmpDict[j][5])
 
+
+
 l = len(util_ts)
 num_labels = 9
 div = math.floor((l - 2) / num_labels)
@@ -60,6 +63,10 @@ for k in range(1, num_labels):
     util_ts_labels.append(util_ts[idx])
 
 util_ts_labels.append(util_ts[l - 1])
+
+
+###################################################################################
+###################################################################################
 
 def avg_lst(lst):
     return sum(lst) / len(lst)
@@ -76,65 +83,62 @@ def median_lst(lst):
 
 median_crv_util = median_lst(crvUtil)
 
-def time_diff(lst):
-    strt = datetime.fromisoformat(lst[0])
-    end = datetime.fromisoformat(lst[len(lst) - 1])
-    return (end - strt).total_seconds()
 
-util_ts_1 = []
-for ts in util_ts:
-    util_ts_1.append(str(ts))
+###################################################################################
+###################################################################################
 
-diff = time_diff(util_ts_1)
-
-secs_in_day = 86400
-days = diff / secs_in_day
-buckets = math.ceil(days)
-
-bucket_diff = diff / 2
-
-util_secs = []
-for x in util_ts:
-    conv = datetime.fromisoformat(str(x)) - datetime(1900,1,1)
-    s = conv.total_seconds()
-    util_secs.append(s)
-
-tsdf = pd.DataFrame({
-    'ts': util_secs,
-    'util': crvUtil
-})
-
-tsdf_1 = tsdf.loc[tsdf['ts'] <= (util_secs[0] + bucket_diff)]
-
-tsdf_2 = tsdf.loc[tsdf['ts'] >= (util_secs[0] + bucket_diff)]
-
-avg_df_1 = avg_lst(tsdf_1['util'])
-avg_df_2 = avg_lst(tsdf_2['util'])
-
-util_ts_labels_sh = []
-for val in util_ts_labels:
-    util_ts_labels_sh.append(val[0:10] + '\n' + val[11:16])
-
+sns.set_theme()
 sns.set_style('whitegrid')
 
+rng_fmt = []
+rng = pd.date_range(start= util_ts[0], end=util_ts[len(util_ts) - 1], periods=12)
+ticks = []
+for val in rng:
+    rng_fmt.append(val.isoformat()[0:10] + '\n' + val.isoformat()[11:16])
+    ticks.append(val.isoformat())
 
-fig = plt.figure(figsize=(18,10))
+fig = plt.figure(figsize=(16,8))
 
 uopt = 0.50
 
-ax = fig.add_axes([0.1,0.1,0.8,0.8])
+u = np.empty(len(util_ts))
+u.fill(uopt)
+a = np.empty(len(util_ts))
+a.fill(avg_crv_util)
 
-ax.plot(util_ts, crvUtil, color='red')
+util_df = pd.DataFrame(crvUtil, index=util_ts, columns=['Utilization'])
+util_df['Util Optimal'] = u
+util_df['Avg Util'] = a
 
-ax.set_xticks(util_ts_labels)
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-ax.set_xticklabels(util_ts_labels_sh)
+ax.plot(util_ts, util_df)
+
+l = len(util_ts)
+twelve = math.floor(l / 12)
+
+func = lambda x, pos: datetime.st
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(twelve))
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(func))
+
+
+# ax.locator_params(axis='x', nbins=12)
+#
+# ax.set_xlim(util_ts[0], util_ts[len(util_ts) - 1])
+#
+# plt.xticks(rng_fmt)
+
+# ax.xaxis.set_minor_locator(AutoMinorLocator())
+
+
+# plt.xticks(ticks, rng_fmt)
+# #
+# # ax.set_xticks(ticks)
+# # ax.set_xticklabels(rng_fmt)
 
 s, e = ax.get_ylim()
-ax.set_yticks(np.arange(round(s, 2),e ,0.0025))
-
-plt.axhline(y = uopt, color = 'green', linestyle = '-')
-plt.axhline(y = avg_crv_util, color = 'yellow', linestyle = '-')
+ax.set_yticks(np.arange(round(s, 2), e, 0.0025))
 
 plt.xlabel('Timestamp')
 plt.ylabel('CRV Utilization Rate')
@@ -142,6 +146,11 @@ plt.ylabel('CRV Utilization Rate')
 ax.legend(['Utilization', 'Optimal Util', 'Avg Utilization'])
 
 plt.savefig('figs/util.png')
+plt.savefig('content/images/util.png')
+
+
+#############################################################################################
+
 
 d = {
     'Timestamp': ts_lst,
@@ -154,113 +163,96 @@ d = {
 
 df = pd.DataFrame(d)
 
-
-def ts_labels_pick(ts):
-    l = len(ts)
-    num_labels = 9
-    div = math.floor((l - 2) / num_labels)
-
-    util_ts_labels = []
-    util_ts_labels.append(ts[0])
-    for k in range(1, num_labels):
-        idx = k * div
-        util_ts_labels.append(ts[idx])
-
-    util_ts_labels.append(ts[l - 1])
-    return util_ts_labels
-
-new_labels = ts_labels_pick(df['Timestamp'])
-
-def ts_labels(ts):
-    util_ts_labels_sh = []
-    for val in ts:
-        util_ts_labels_sh.append(val[0:10] + '\n' + val[11:16])
-    return util_ts_labels_sh
-
-new_labels_final = ts_labels(new_labels)
-
-
-sns.set_style('whitegrid')
-
-fig1 = plt.figure(figsize=(18,10))
+fig1 = plt.figure(figsize=(12,6))
 
 ax1 = fig1.add_axes([0.1,0.1,0.8,0.8])
 
-# ax1.plot(df['Timestamp'], df['PT Px'], color='red')
-ax1.plot(df['Timestamp'], df['YT Px'], color='blue')
+len_df = len(df['Timestamp'])
+rng_ts = pd.date_range(start= df['Timestamp'][0], end=df['Timestamp'][len_df - 1], periods=12)
+rng_ticks = []
+rng_ts_fmt = []
+for val in rng_ts:
+    rng_ts_fmt.append(val.isoformat()[0:10] + '\n' + val.isoformat()[11:16])
+    rng_ticks.append(val.isoformat())
+
+ax1.plot(df['Timestamp'], df['YT Px'], color='deepskyblue')
 
 s, e = ax1.get_ylim()
 ax1.set_yticks(np.arange(round(s, 2) - 0.001,e + 0.001,0.0005))
 
+# plt.xticks(rng_ts_fmt, rng_ticks)
 
-plt.xticks(new_labels, new_labels_final)
+ax.set_xticks(rng_ticks)
+ax.set_xticklabels(rng_ts_fmt)
+
 
 plt.xlabel('Timestamp')
 plt.ylabel('YT Price')
 
 plt.savefig('figs/yt_price.png')
+plt.savefig('content/images/yt_price.png')
 
-plt.show()
-#%%
-sns.set_style('whitegrid')
 
-fig1 = plt.figure(figsize=(18,10))
+fig1 = plt.figure(figsize=(12,6))
 
 ax1 = fig1.add_axes([0.1,0.1,0.8,0.8])
 
 # ax1.plot(df['Timestamp'], df['PT Px'], color='red')
-ax1.plot(df['Timestamp'], df['PT Px'], color='green')
+ax1.plot(df['Timestamp'], df['PT Px'], color='springgreen')
 
 s, e = ax1.get_ylim()
-ax1.set_yticks(np.arange(round(s, 2) + 0.0025,e + 0.0025,0.0025))
+ax1.set_yticks(np.arange(round(s, 2) + 0.0025,e + 0.0025,0.00125))
 
 
-plt.xticks(new_labels, new_labels_final)
+ax.set_xticks(rng_ticks)
+ax.set_xticklabels(rng_ts_fmt)
 
 plt.xlabel('Timestamp')
 plt.ylabel('PT Price')
 
 plt.savefig('figs/pt_price.png')
+plt.savefig('content/images/pt_price.png')
 
-sns.set_style('whitegrid')
 
-fig1 = plt.figure(figsize=(18,10))
+fig1 = plt.figure(figsize=(12,6))
 
 ax1 = fig1.add_axes([0.1,0.1,0.8,0.8])
 
 # ax1.plot(df['Timestamp'], df['PT Px'], color='red')
-ax1.plot(df['Timestamp'], df['LongYield APY'], color='yellow')
+ax1.plot(df['Timestamp'], df['LongYield APY'], color='deeppink')
 
 s, e = ax1.get_ylim()
 ax1.set_yticks(np.arange(round(s, 2),e,0.05))
 
 
-plt.xticks(new_labels, new_labels_final)
+ax.set_xticks(rng_ticks)
+ax.set_xticklabels(rng_ts_fmt)
 
 plt.xlabel('Timestamp')
 plt.ylabel('Long Yield APY')
 
 plt.savefig('figs/longYield.png')
+plt.savefig('content/images/longYield.png')
 
-sns.set_style('whitegrid')
 
-fig1 = plt.figure(figsize=(18,10))
+
+fig1 = plt.figure(figsize=(14,7))
 
 ax1 = fig1.add_axes([0.1,0.1,0.8,0.8])
 
 # ax1.plot(df['Timestamp'], df['PT Px'], color='red')
-ax1.plot(df['Timestamp'], df['Underlying APY'], color='red')
-ax1.plot(df['Timestamp'], df['Implied APY'], color='blue')
-
-
+ax1.plot(df['Timestamp'], df['Underlying APY'], color='orangered')
+ax1.plot(df['Timestamp'], df['Implied APY'], color='cyan')
 
 s, e = ax1.get_ylim()
 ax1.set_yticks(np.arange(round(s, 2),e,0.0025))
 
 
-plt.xticks(new_labels, new_labels_final)
+ax.set_xticks(rng_ticks)
+ax.set_xticklabels(rng_ts_fmt)
 
 plt.xlabel('Timestamp')
 plt.ylabel('APY')
 
 plt.savefig('figs/underlying_implied.png')
+plt.savefig('content/images/underlying_implied.png')
